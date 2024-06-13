56a4e348505ea15f0c9e48951ff51a07363447a9
Sergio Melero <sergiomele97@gmail.com>
2024-06-13 13:22:53 +0200

    Se consigue imprimir un sprite por pantalla

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pyboy import PyBoy
 pyboy = PyBoy('ROMs/pokemon_crystal_ingles.gbc', sound=True)
+with open("Pruebas/state_file.state", "rb") as f:
+    pyboy.load_state(f)
 while pyboy.tick():
     pass
 pyboy.stop()
