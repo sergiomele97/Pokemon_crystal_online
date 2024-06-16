e789fed2b93259c6a35f57c01b0e23933249c6eb
Sergio Melero <sergiomele97@gmail.com>
2024-06-16 19:23:03 +0200

    se modifica libreria

--- Pruebas/Prueba8.py
+++ Pruebas/Prueba8.py
@@ -1,7 +1,7 @@
 from pyboy import PyBoy
 
 pyboy = PyBoy('ROMs/pokemon_crystal_ingles.gbc', window="headless", sound_emulated=True, sound=True)
-pyboy.set_emulation_speed(2)
+pyboy.set_emulation_speed(1)
 
 while pyboy.tick():
     pass
