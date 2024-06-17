406e8c1b7439db7018d5340e4a1edd6a6efb37c5
Sergio Melero <sergiomele97@gmail.com>
2024-06-17 18:55:18 +0200

    Se implemente satisfactoriamente un frame limiter en w null pyboy

--- Pruebas/Prueba8.py
+++ Pruebas/Prueba8.py
@@ -1,6 +1,6 @@
 from pyboy import PyBoy
 
-pyboy = PyBoy('ROMs/pokemon_crystal_ingles.gbc', window="headless", sound_emulated=True, sound=True)
+pyboy = PyBoy('ROMs/pokemon_crystal_ingles.gbc', window="null", sound_emulated=True, sound=True)
 pyboy.set_emulation_speed(1)
 
 while pyboy.tick():
