1a6711ca7d720255cfc4bd437c9f58d61e793b48
Sergio Melero <sergiomele97@gmail.com>
2024-06-30 20:39:06 +0200

    Ligero refactor

--- emulator.py
+++ emulator.py
@@ -83,8 +83,13 @@ class Emulator:
         sprite_factory = sdl2.ext.SpriteFactory(sdl2.ext.TEXTURE, renderer=self.renderer)
         sprite = sprite_factory.from_image("Resources/mainOW.bmp")
 
-        # Debug emular player
-        #   x_draw = (x jugador2 - x jugador1 + cuadrados hasya centro pantalla) * pixeles/cuadrado
+        ########################### DEBUG
+
+        # Corrección: para actualizar el sprite con precisión de pixel y no de cuadrado
+
+
+
+        #   x_draw = (x jugador2 - x jugador1 + cuadrados hasta centro pantalla) * pixeles/cuadrado
 
         x_draw = (self.player2.x_coord - self.player.x_coord + 4) * 80
         y_draw = (self.player2.y_coord - self.player.y_coord + 4) * 80 - 20
@@ -92,6 +97,7 @@ class Emulator:
         # 2. Render
         self.renderer.copy(sprite, srcrect=(17, 0, 16, 16), dstrect=(x_draw, y_draw, 80, 80))  # Sprite y rectangulo animacion
 
+        ########################### DEBUG
 
     def update_background(self):
         # 1. Gets emulator image as ndarray
@@ -137,14 +143,16 @@ class Emulator:
     def get_player_coords(self):
         self.player.x_coord = self.pyboy.memory[0xDCB8]
         self.player.y_coord = self.pyboy.memory[0xDCB7]
+        self.player.x_coord_sprite = self.pyboy.memory[0xD14C]
+        self.player.y_coord_sprite = self.pyboy.memory[0xD14D]
         self.player.map_number = self.pyboy.memory[0xDCB6]
         self.player.map_bank = self.pyboy.memory[0xDCB5]
 
         print("")
-        print(self.pyboy.memory[0xD14C])    # sprite X
-        print(self.pyboy.memory[0xD14D])    # sprite Y
-        print(self.player.x_coord)
-        print(self.player.y_coord)
+        print("sprite X = ", self.pyboy.memory[0xD14C])    # sprite X
+        print("sprite Y = ", self.pyboy.memory[0xD14D])    # sprite Y
+        print("player X = ", self.player.x_coord)
+        print("player Y = ", self.player.y_coord)
         print("map bank = ", self.pyboy.memory[0xDCB5])
         print("map number = ", self.pyboy.memory[0xDCB6])
 
--- player.py
+++ player.py
@@ -3,5 +3,7 @@
 class Player:
     x_coord = 0
     y_coord = 0
+    x_coord_sprite = 0
+    y_coord_sprite = 0
     map_bank = 0
     map_number = 0
