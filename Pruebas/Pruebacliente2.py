9c9d38a81b91e3a04dafe2ccdef68366898c0d59
Sergio Melero <sergiomele97@gmail.com>
2024-07-10 20:28:28 +0200

    servidorcito

--- Pruebas/Pruebacliente2.py
+++ Pruebas/Pruebacliente2.py
@@ -1,15 +1,13 @@
-import socket
+import requests
 
-# Configurar el cliente
-HOST = '78f5f28d-5168-4c5e-b796-ce64ef2dd8f0-00-3w4d6lfb3rjai.riker.replit.dev'
-PORT = 8080
+# Dirección y puerto del servidor
+HOST = 'https://confident-gratia-pokemon-crystal-online-d49b6bc2.koyeb.app/pks'  # Reemplaza con tu URL de Replit
+PORT = 8000
+URL = f"{HOST}/mensaje"
 
-# Crear un socket TCP/IP
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    # Conectar al servidor
-    client_socket.connect((HOST, PORT))
+# Mensaje a enviar
+message = "Hola, servidor"
 
-    # Enviar datos al servidor
-    message = "Hola, servidor!"
-    client_socket.sendall(message.encode())
-    print(f'Mensaje enviado al servidor: {message}')
+# Enviar el mensaje al servidor
+response = requests.post(URL, data=message)
+print(f"Respuesta del servidor: {response.text}")
